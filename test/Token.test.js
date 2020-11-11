91414c1c52248d8b1e664f602f8f9da9708665b4
Adolfo Reyes <adolfo0620@gmail.com>
2020-11-10 22:47:45 -0500

    adding transferFrom method and its testcase

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,7 +5,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token', ([deployer, receiver]) => {
+contract('Token', ([deployer, receiver, exchange]) => {
     const name = 'Wario Token'
     const symbol = 'WARIO'
     const decimals = '18'
@@ -87,5 +87,89 @@ contract('Token', ([deployer, receiver]) => {
                 await token.transfer(0x0, amount, {from:deployer}).should.be.rejected
             })
         })
+    })
+
+    describe('approving tokens', () => {
+        let result
+        let amount
+
+        beforeEach(async () => {
+            amount = tokens(100)
+            result = await token.approve(exchange, amount, {from: deployer})
+        })
+
+        describe('success', () => {
+            it('allocates an allowance for delegated taken spending on exhange', async () => {
+                const allownace = await token.allowance(deployer, exchange)
+                allownace.toString().should.equal(amount.toString())
+            })
+            it('emits a approval event', async () => {
+                const log  = result.logs[0]
+                log.event.should.eq('Approval')
+                const event = log.args
+                event.owner.toString().should.equal(deployer, 'owner is correct')
+                event.spender.should.equal(exchange, 'spender is correct')
+                event.value.toString().should.equal(amount.toString(), 'value is correct')
+            })
+        })
+        
+        describe('failure', () => {
+            it('rejects invaluid spender', async () => {
+                await token.approve(0x0, amount, {from: deployer}).should.be.rejected
+            })
+        })
+
+        describe('delegated token transfers', () => {
+            let amount, result
+
+            beforeEach(async () => {
+                amount = tokens(100)
+                await token.approve(exchange, amount, {from: deployer})
+            })
+    
+            describe('success', async ()=> {
+                beforeEach(async () => {
+                    result = await token.transferFrom(deployer, receiver, amount, {from: exchange})
+                })
+                it('transfers tokens balances', async () => {
+                    let balanceOf
+                    // Transfer
+                    balanceOf = await token.balanceOf(deployer)
+                    balanceOf.toString().should.equal(tokens(999900).toString())
+                    balanceOf = await token.balanceOf(receiver)
+                    balanceOf.toString().should.equal(tokens(100).toString())
+        
+                })
+                
+                it('resets the allowance', async () => {
+                    const allownace = await token.allowance(deployer, exchange)
+                    allownace.toString().should.equal('0')
+                })
+        
+                it('emits a transfer event', async () => {
+                    const log  = result.logs[0]
+                    log.event.should.eq('Transfer')
+                    const event = log.args
+                    event.from.toString().should.equal(deployer, 'from is correct')
+                    event.to.should.equal(receiver, 'to is correct')
+                    event.value.toString().should.equal(amount.toString(), 'value is correct')
+                })
+            })
+    
+            describe('failure', async () => {
+                it('rejects insufficient balances', async () => {
+                    const invalidAmount = tokens(1000000000)
+                    await token.transferFrom(deployer, receiver, invalidAmount, {from: exchange}).should.be.rejectedWith(EVM_REVERT);
+        
+                })
+    
+                it('rejects invalid recipients', async () => {
+                    await token.transferFrom(deployer, 0x0, amount, {from: exchange}).should.be.rejected
+                })
+            })
+        })
+        
+        
+
     })
 })
