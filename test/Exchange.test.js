2c4b089a63824fff1fbbce975c077a544932fdf8
Adolfo Reyes <adolfo0620@gmail.com>
2020-11-13 21:49:27 -0500

    adding testcase for order and cancel order

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
     let exchange
     let token
     const feePercent = 10
@@ -184,6 +184,78 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
                 result.toString().should.equal(ether(1).toString())
             })
         })
+
+        describe('making orders', async () => {
+            let result
+            beforeEach(async () => {
+                result = await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), {from: user1})
+            })
+            it('tracks the newly created order', async () => {
+                const orderCount = await exchange.orderCount()
+                orderCount.toString().should.equal('1')
+                const order = await exchange.orders('1')
+                order.id.toString().should.equal('1', 'id is correct')
+                order.user.should.equal(user1, 'user is correct')
+                order.tokenGet.should.equal(token.address, 'tokenGet is correct')
+                order.amountGet.toString().should.equal(tokens(1).toString())
+                order.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+                order.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+                order.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+            })
+            it('emits an order event',async () => {
+                const log = result.logs[0]
+                log.event.should.eq('Order')
+                const event = log.args
+                event.id.toString().should.equal('1', 'id is correct')
+                event.user.should.equal(user1, 'user is correct')
+                event.tokenGet.should.equal(token.address, 'tokenGet is correct')
+                event.amountGet.toString().should.equal(tokens(1).toString(), 'amountGet is correct')
+                event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGIve is correct')
+                event.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+                event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+            })
+        })
+        describe('order actions', async () => {
+            beforeEach( async () => {
+                await exchange.depositEther({from: user1, value: ether(1)})
+                await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), {from: user1})
+            })
+            describe('cancelling orders', async () => {
+                let result
+                describe('success', async () => {
+                    beforeEach(async () => {
+                        result = await exchange.cancelOrder('1', {from: user1})
+                    })
+                    it('updates cancelled orders', async () => {
+                        const orderCancelled = await exchange.orderCancelled(1)
+                        orderCancelled.should.equal(true)
+                    })
+                    it('amits a cancel event', async () => {
+                        const log = result.logs[0]
+                        log.event.should.eq('Cancel')
+                        const event = log.args
+                        event.id.toString().should.equal('1', 'id is correct')
+                        event.user.should.equal(user1, 'user is correct')
+                        event.tokenGet.should.equal(token.address, 'tokenGet is correct')
+                        event.amountGet.toString().should.equal(tokens(1).toString(), 'amountGet is correct')
+                        event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+                        event.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+                        event.timestamp.toString().length.should.be.at.least(1,'timestamp is present')
+                    })
+                })
+                describe('failure', async () => {
+                    it('rejects invalid order ids', async () => {
+                        const invalidOrderId = 99999
+                        await exchange.cancelOrder(invalidOrderId, {from: user1}).should.be.rejectedWith(EVM_REVERT)
+                    })
+                    it('rejects unauthorized cancelations', async () => {
+                        await exchange.cancelOrder('1', { from: user2 }).should.be.rejectedWith(EVM_REVERT)
+                    })
+                })
+            })
+
+        })
+        
     })
 
 })
