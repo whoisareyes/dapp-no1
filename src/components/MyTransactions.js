18e1a612ce521cd20cb96f9f4ce113d1198d04d6
Adolfo Reyes <adolfo0620@gmail.com>
2020-11-21 00:18:32 -0500

    adding transaction component

---
+++ src/components/MyTransactions.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Tabs, Tab } from 'react-bootstrap'
+
+class MyTransactions extends Component {
+    render(){
+        return (
+            <div className="card bg-dark text-white">
+                <div className="card-header">
+                    My Transaction
+                    <div className="card-body">
+                        <Tabs defaultActiveKey="trades" className="bg-dark text-white">
+                            <Tab eventKey="trades" title="Trades" className="bg-dark">
+                                <table className="table table-dark table-sm small">
+                                    <thead>
+                                        <tr>
+                                            <th>Time</th>
+                                            <th>Dapp</th>
+                                            <th>Dapp/ETH</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </Tab>
+                            <Tab eventKey="orders" title="Orders">
+                                <table className="table table-dark table-sm small">
+                                    <thead>
+                                        <tr>
+                                            <th>Amount</th>
+                                            <th>DAPP/ETH</th>
+                                            <th>Cancel</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </Tab>
+                        </Tabs>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
