eaa5271e498c990c0febf5dcbb0843761b815617
Adolfo Reyes <adolfo0620@gmail.com>
2020-11-20 23:15:09 -0500

    feat: displaying open buy and sell orders

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,4 +1,4 @@
-import { get } from 'lodash'
+import { get, reject, groupBy } from 'lodash'
 import { createSelector } from 'reselect'
 import { ETHER_ADDRESS, tokens, ether, GREEN, RED } from '../helpers'
 import moment from 'moment'
@@ -50,7 +50,7 @@ const decorateFilledOrders = (orders) => {
 const decorateOrder = (order) => {
     let etherAmount
     let tokenAmount
-    if(order.tokenGive == ETHER_ADDRESS){
+    if(order.tokenGive === ETHER_ADDRESS){
         etherAmount = order.amountGive
         tokenAmount = order.amountGet
     }else {
@@ -58,8 +58,8 @@ const decorateOrder = (order) => {
         tokenAmount = order.amountGive
     }
     const precision = 100000
-    let tokenPrice = (etherAmount / tokenAmount)
-    tokenPrice = Math.round(tokenPrice * precision) / precision
+    let tokenPrice= (etherAmount / tokenAmount)
+    tokenPrice= Math.round(tokenPrice* precision) / precision
 
     return ({
         ...order,
@@ -69,20 +69,83 @@ const decorateOrder = (order) => {
         formattedTimestamp: moment.unix(order.timestamp).format('h:mm:ss a M/D')
     })
 }
+
+const allOrdersLoaded = state => get(state, 'exchange.allOrders.loaded', false)
+const allOrders = state => get(state, 'exchange.allOrders.data', [])
+
+const cancelledOrdersLoaded = state => get(state, 'exchange.cancelledOrders.loaded', false)
+export const cancelledOrdersLoadedSelector = createSelector(cancelledOrdersLoaded, loaded => loaded)
+
+const cancelledOrders = state => get(state, 'exchange.cancelledOrders.data', [])
+export const cancelledOrdersSelector = createSelector(cancelledOrders, o => o)
+
 const decorateFilledOrder = (order, previousOrder) => {
     return({
         ...order,
-        tokenPriceClass: tokenPriceClass(order.tokenPice, order.id, previousOrder)
+        tokenPricelass: tokenPricelass(order.tokenPice, order.id, previousOrder)
     })
 }
 
-const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
+const tokenPricelass = (tokenPrice, orderId, previousOrder) => {
     if(previousOrder.id === orderId){
         return GREEN
     }
-    if(previousOrder.tokenPrice <= tokenPrice){
+    if(previousOrder.tokenPrice<= tokenPrice){
         return GREEN
     }else{
         return RED
     }
+}
+
+const openOrders = state => {
+    const all = allOrders(state)
+    const filled = filledOrders(state)
+    const cancelled = cancelledOrders(state)
+
+    const openOrders = reject(all, (order) => {
+        const orderFilled = filled.some((o) => o.id === order.id)
+        const orderCancelled = cancelled.some((o) => o.id === order.id)
+        return(orderFilled || orderCancelled)
+    })
+
+    return (openOrders)
+}
+
+const orderBookLoaded = state => cancelledOrdersLoaded(state) && filledOrdersLoaded(state) && allOrdersLoaded(state)
+export const orderBookLoadedSelector = createSelector(orderBookLoaded, b => b)
+
+export const orderBookSelector = createSelector (
+    openOrders,
+    (orders) => {
+        orders = decoratedOrderBookOrders(orders)
+        orders = groupBy(orders, 'orderType')
+        const buyOrders = get(orders, 'buy', [])
+        const sellOrders = get(orders, 'sell', [])
+        orders = {
+            ...orders,
+            buyOrders: buyOrders.sort((a,b) => b.tokenPrice - a.tokenPrice),
+            sellOrders: sellOrders.sort((a,b) => b.tokenPrice - a.tokenPrice)
+        }
+        return orders
+    }
+)
+
+const decoratedOrderBookOrders = (orders) => {
+    return (
+        orders.map((order) => {
+            order = decorateOrder(order)
+            order = decorateOrderBookOrder(order)
+            return(order)
+        })
+    )
+}
+
+const decorateOrderBookOrder =(order) => {
+    const orderType = order.tokenGive === ETHER_ADDRESS ? 'buy' : 'sell'
+    return({
+        ...order,
+        orderType,
+        orderTypeClass: (orderType === 'buy' ? GREEN : RED),
+        orderFillClass: orderType === 'buy' ? 'sell' : 'buy'
+    })
 }
